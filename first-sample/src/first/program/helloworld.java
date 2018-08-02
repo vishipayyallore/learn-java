4a5f4bd101385400ac5b7b13320f472926df4ac3
Viswanatha Swamy <vishipayyallore@yahoo.com>
2018-08-02 19:23:09 +0530

    Removed the commented line.

--- first-sample/src/first/program/helloworld.java
+++ first-sample/src/first/program/helloworld.java
@@ -3,7 +3,6 @@ package first.program;
 public class helloworld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Hello World. \nThis is my first program in Java!");
 	}
 
