2a7bd7ae7848eab8a6c8bef67039135afcaf765e
Viswanatha Swamy <vishipayyallore@yahoo.com>
2018-08-04 09:54:13 +0530

    Sample Program with Interface

---
+++ data-types-demo/src/com/datatypes/demo/program/Program.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.datatypes.demo.program;
+
+/**
+ * @author Viswanatha Swamy
+ *
+ */
+public class Program {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
