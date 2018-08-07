0a756ccec4c32fb21c5fb460299a1deb4b75d0d3
Viswanatha Swamy <vishipayyallore@yahoo.com>
2018-08-07 22:55:55 +0530

    ..

---
+++ RestfulWebApi/async-rest/src/main/java/com/pluralsight/BookResource.java
@@ -0,0 +1,27 @@
+package com.pluralsight;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.Collection;
+
+@Path("/books")
+public class BookResource {
+
+    BookDao dao = new BookDao();
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Collection<Book> getBooks() {
+        return(dao.getBooks());
+    }
+
+    @Path("/{id}")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Book getBook(@PathParam("id") String id) {
+        return(dao.getBook(id));
+    }
+}
