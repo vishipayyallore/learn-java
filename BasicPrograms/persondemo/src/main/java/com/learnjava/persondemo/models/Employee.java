9b6d4bb91ffada25aa924b2d745c978f0885431f
Viswanatha Swamy <vishipayyallore@yahoo.com>
2018-09-12 21:00:30 +0530

    Core Refactoring

--- BasicPrograms/persondemo/src/main/java/com/learnjava/persondemo/models/Employee.java
+++ BasicPrograms/persondemo/src/main/java/com/learnjava/persondemo/models/Employee.java
@@ -14,17 +14,17 @@ public class Employee implements IEmployee {
 
 	public UUID Id = UUID.randomUUID();
 	
-	public String FirstName = "No Name";
+	public String FirstName;
 	
-	public String LastName = "No Name";
+	public String LastName;
 	
-	public byte Age = 18;
+	public byte Age;
 	
-	public float Salary = 25000.00f;
+	public float Salary;
 	
-	public static String Department = "IT";
+	public static String Department;
 	
-	public Project ProjectAssigned = new Project();
+	public Project ProjectAssigned;
 	
 	
 	public Employee(){
@@ -37,8 +37,15 @@ public class Employee implements IEmployee {
 		FirstName = firstName;
 	}
 	
+	// Instance Initializer Block
 	{ 
 		System.out.println("---------- Instance Initializer Block of Employee ----------");
+		FirstName = "Employee First Name";
+		LastName = "Employee Last Name";
+		Age = 21;
+		Salary = 25000.00f;
+		Department = "IT";
+		
 		ProjectAssigned = new Project("Distributed Cache", LocalDate.of(2019, 12, 01));
 	}
 	
