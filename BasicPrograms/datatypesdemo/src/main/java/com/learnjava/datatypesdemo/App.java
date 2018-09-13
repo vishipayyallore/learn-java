9c169333b04b5650412c8c6e7ba9dec3f1c7ee86
Viswanatha Swamy <vishipayyallore@yahoo.com>
2018-09-14 00:07:46 +0530

    Added lambda expression as function parameter

--- BasicPrograms/datatypesdemo/src/main/java/com/learnjava/datatypesdemo/App.java
+++ BasicPrograms/datatypesdemo/src/main/java/com/learnjava/datatypesdemo/App.java
@@ -3,34 +3,44 @@ package com.learnjava.datatypesdemo;
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
 import java.util.Locale;
+import java.util.function.Consumer;
 
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "***** Data Types Demo *****" );
-        System.out.printf( "%,.2f", 12345678.9 );
-        
-        Locale locale  = new Locale("en", "IN");
-        String pattern = "#,###.##";
+public class App {
+	public static void main(String[] args) {
+		//
+		Consumer<String> printString = message -> System.out.println(message);
 
-        DecimalFormat decimalFormat = (DecimalFormat)
-                NumberFormat.getNumberInstance(locale);
-        decimalFormat.applyPattern(pattern);
+		System.out.println(String.format("***** Data Types Demo in (%s)*****", System.getProperty("user.dir")));
+		System.out.printf("%,.2f", 12345678.9);
 
-        String format = decimalFormat.format(123456789.123);
-        System.out.println(String.format("\nSalary= %s", format));
-        
-        // Basic Lambda Expression
-        new Thread(()-> 
-        {System.out.println("New thread created");}).start();
-        
-        // lambda expression to define the calculate method 
-        ISquare square = (int x) -> x*x;
-        System.out.println(String.format("Square of 5 is %s", square.calculate(5)));
-    }
+		Locale locale = new Locale("en", "IN");
+		String pattern = "#,###.##";
+
+		DecimalFormat decimalFormat = (DecimalFormat) NumberFormat.getNumberInstance(locale);
+		decimalFormat.applyPattern(pattern);
+
+		String format = decimalFormat.format(123456789.123);
+		System.out.println(String.format("\nSalary= %s", format));
+
+		// Basic Lambda Expression
+		new Thread(() -> {
+			System.out.println("New thread created");
+		}).start();
+
+		// lambda expression to define the calculate method
+		ISquare square = (int x) -> x * x;
+		System.out.println(String.format("Square of 5 is %s", square.calculate(5)));
+
+		executeLambda("Hello Function Expresseion", printString);
+		executeLambda("Hello Function Again", (message) -> System.out.println(message));
+	}
+
+	private static void executeLambda(String value, Consumer<String> method) {
+		method.accept(value);
+	}
+
 }
